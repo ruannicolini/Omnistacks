12fc60b6ca7b42a5dae5566bcd5f34ebc3b50d0c
ruannicolini <ruan_03@hotmail.com>
2020-03-29 16:05:07 -0300

    Inicio: Frontend Be The Hero

--- Omnistack 11/Frontend/src/App.js
+++ Omnistack 11/Frontend/src/App.js
@@ -1,28 +1,34 @@
-import React, { useState } from 'react';
+import React from 'react';
+import './global.css';
 
 /* Um Componente no react é uma função com html, que pode ter comportamento javascript, css */
 /* Quando um html esta escrito dentro de um javascript, chamamos de jsx */
 
-import Header from './Header';
+import Logon from './Pages/Logon';
 
 function App() {
+
+  return (
+    <Logon />    
+  );
 
   //counter é um estado, o que conceitualmente ao se alterar, atualizará o valor em todo lugar onde é renderizado.
   //para inicializar um estado, utilizamos useState passando por parametro o valor inicial
   // useState retorna um array de 2 posiçoes, onde no primeiro index temos o valor inicial
   // e no segundo index uma função para arualizar o valor.
-  const [counter, setCounter] = useState(0);
+  // const [counter, setCounter] = useState(0);
 
-  function increment(){
-    setCounter(counter+1);
-  }
+  // function increment(){
+  //   setCounter(counter+1);
+  // }
 
-  return ( 
-    <div>
-      <Header>Contador = {counter}</Header>
-      <button onClick={increment} >Incrementar</button>
-    </div>    
-  );
+  // return ( 
+  //   <div>
+  //     <Header>Contador = {counter}</Header>
+  //     <button onClick={increment} >Incrementar</button>
+  //   </div>    
+  // );
+
 }
 
 export default App;
