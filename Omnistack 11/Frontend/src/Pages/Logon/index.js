5e909c19d0f0bcce921415852c0571451b9004eb
ruannicolini <ruan_03@hotmail.com>
2020-03-29 20:42:32 -0300

    Logon page add

--- Omnistack 11/Frontend/src/Pages/Logon/index.js
+++ Omnistack 11/Frontend/src/Pages/Logon/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import './styles.css';
+import { FiLogIn } from 'react-icons/fi'
+
+import logoImg from '../../assets/logo.svg';
+import heroesImg from '../../assets/heroes.png';
 
 export default function Logon(props) {
     return (
-        <header>  
-            <h1>Logon</h1>      
-        </header>        
+        <div className="logon-container">
+            <section className="form">
+                <img src={logoImg} alt="Be The Hero"/>
+                <form>
+                    <h1>Faça seu logon</h1>    
+                    <input placeholder="Sua Id" />
+                    <button className="button" type="submit">Entrar</button>
+                    <a href="/register" >
+                        <FiLogIn size={16} color="#E02041" />
+                        Não tenho cadastro
+                    </a>
+                </form>
+            </section>
+            <img src={heroesImg} alt="Heroes"/>
+        </div>
+        
     );
 }
